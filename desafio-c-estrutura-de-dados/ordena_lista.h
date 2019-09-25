adeb1f1f93bc375bd27eb202f8a4502c58399da0
FortesThiago <48772859+FortesThiago@users.noreply.github.com>
2019-09-25 19:43:09 -0300

    Update ordena_lista.h

--- desafio-c-estrutura-de-dados/ordena_lista.h
+++ desafio-c-estrutura-de-dados/ordena_lista.h
@@ -1,7 +1,25 @@
 void ordenar(LISTAENC *lista) {
-    while (lista != NULL) {
-        printf("Cor: %s, tamanho: %s, dono: %s\n", lista->info.cor, lista->info.tamanho, lista->info.nome_dono);
-        lista = lista->prox;
+    if( *lista == NULL )                                // verifica se a lista esta vazia 
+        printf( "\n Lista vazia!" );
+    else {
+        if( (*lista)->prox == NULL )                    // verifica se a lista esta vazia 
+             printf( "\n Lista com apenas 1 elemento!" );
+        else{
+             INFORMACAO aux;                        // auxiliar para a troca de dados
+             LISTAENC *ant = *lista;                        // posiciona ponteiro auxiliar para anterior no início da lista
+             LISTAENC *p   = ant->prox;                 //posiciona ponteiro auxiliar que irá caminhar pela lista para comparar com ant
+             while( ant->prox != NULL ){            // enquanto não for final de lista 
+                    while( p != NULL ){             // enquanto não compara ant com todos posteriores
+                           if( p->info.cor < ant->info.cor || p->info.tamanho < ant->info.tamanho || p->info.nome_dono < p->info.nome_dono){
+                               aux      = p->info;  // faz a troca (algoritmo de ordenação Bolha)
+                               p->info  = ant->info;
+                               ant->info= aux;
+                           } // fim if
+                           p= p->prox;
+                    } // fim while
+                    ant= ant->prox;                 // atualiza ponteiros
+                    p= ant->prox;  
+             } // fim while                    
+         } // fim else
     }
-    printf("\n");
-};
+}
